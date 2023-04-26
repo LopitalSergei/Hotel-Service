5deb6566c7a80bd6cee9c1229d97869e6c570f32
Laptev S <sl260401@gmail.com>
2023-04-26 11:41:16 +0300

    header with responsive design v1

---
+++ js/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/* Burger menu */
+const burger = document.getElementById('burger');
+burger.addEventListener('click', burgerMenu);
+function burgerMenu() {
+	burger.classList.toggle('burger-active');
+	document.getElementsByClassName('main-menu')[0].classList.toggle('burger-active');
+	document.body.classList.toggle('lock');
+}
+/*-------------*/
+
+/* Menu button */
+
+if (window.innerWidth <= 565) {
+	const menuBtn = document.getElementsByClassName('header__nav-right')[0];
+	const menulinks = document.getElementsByClassName('main-menu__links')[0];
+	menulinks.append(menuBtn);
+}
+
+window.addEventListener('resize', function (event) {
+	if (window.innerWidth <= 565) {
+		const menuBtn = document.getElementsByClassName('header__nav-right')[0];
+		const menulinks = document.getElementsByClassName('main-menu__links')[0];
+		menulinks.append(menuBtn);
+	} else {
+		const menuBtn = document.getElementsByClassName('header__nav-right')[0];
+		const header__burger = document.getElementsByClassName('header__burger')[0];
+		header__burger.before(menuBtn);
+	}
+}, true);
+
+
+
+/*------------*/
